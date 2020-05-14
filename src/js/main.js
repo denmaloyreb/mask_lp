4e21dda5453a48159657f58026c87a1cea412113
DenisRebenock <denmaloyreb@mail.ru>
2020-05-14 18:36:17 +0300

    refactor: add files from another folder

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.slider').slick({
+        arrows: true,
+        fade: true,
+        autoplay: false,
+        dots: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+
+
+    });
+
+
+
+});
